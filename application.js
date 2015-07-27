809da137351433b1072040481d135be550b303a0
Logan Schanbacher <schogan@localhost.localdomain>
2015-07-27 19:42:56 -0400

    yes

---
+++ application.js
@@ -0,0 +1,9 @@
+       function weekSeven(){
+             var Itsabox = document.getElementById("Itsabox");
+             var content = document.getElementById("output");
+             content.innerHTML = "";
+             for(var i = 100; i>=0; i=i-1){
+                 content.innerHTML += '<div>' + Itsabox.value + '</div>'
+                }
+        }
+	
